339c8914ed912473814250cbc40e9f79c0593592
Glenn Garcia <glenngarcia0817@gmail.com>
2026-04-26 17:51:43 -0400

    Add files via upload

---
+++ index_milestone.c
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Midterm Milestone</title>
+  <link rel="stylesheet" href="../../../css/style.css">
+</head>
+
+<body>
+
+<div class="header">
+  <h1>Midterm Milestone</h1>
+  <p>Senior Design Group 20</p>
+</div>
+
+<div class="navbar">
+  <a href="../../../index.html">Home</a>
+  <a href="../../../docs/">Documents</a>
+  <a href="../../../videos/">Videos</a>
+</div>
+
+<div style="padding:20px;">
+  <h2>Document</h2>
+
+  <p>
+    <a href="Midterm_Report.pdf" target="_blank">
+      Open PDF in New Tab
+    </a>
+  </p>
+
+  <embed src="Midterm_Report.pdf"
+         type="application/pdf"
+         width="100%"
+         height="900px" />
+</div>
+
+</body>
+</html>
